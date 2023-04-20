4c34210f6dd3a446f7d894817e93cff51960838a
SosaiKyrosai <sosaikurosai@gmail.com>
2023-04-20 11:30:55 +0700

    Modern main structure

--- Cet/Cet/settings.py
+++ Cet/Cet/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'CetApp',
+    # 'generateName',
+    # 'estimateName',
+    # 'patCat',
+    # 'arrangeTheCats',
 ]
 
 MIDDLEWARE = [
