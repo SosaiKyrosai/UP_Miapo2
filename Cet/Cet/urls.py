1becab78d1a3e2c21df22aaf5f8717540f2f2546
SosaiKyrosai <sosaikurosai@gmail.com>
2023-04-20 11:44:02 +0700

    Modern main structure

--- Cet/Cet/urls.py
+++ Cet/Cet/urls.py
@@ -4,5 +4,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('generate/', include('generateName.urls')),
+    # path('estimate/', include('estimateName.urls')),
+    # path('pat/', include('patCat.urls')),
+    # path('arrange/', include('arrangeTheCats.urls')),
     path('', include('CetApp.urls')),
 ]
