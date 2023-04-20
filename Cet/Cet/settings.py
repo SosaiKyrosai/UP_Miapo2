b2049b6f04c5b2cda971c726ebfe5118c3a2c3fc
SosaiKyrosai <sosaikurosai@gmail.com>
2023-04-20 13:05:29 +0700

    New function - estimate

--- Cet/Cet/settings.py
+++ Cet/Cet/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'core',
     'CetApp',
     # 'generateName',
-    # 'estimateName',
+    'estimateName',
     # 'patCat',
     # 'arrangeTheCats',
 ]
--- Cet/Cet/urls.py
+++ Cet/Cet/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('generate/', include('generateName.urls')),
-    # path('estimate/', include('estimateName.urls')),
+    path('estimate/', include('estimateName.urls')),
     # path('pat/', include('patCat.urls')),
     # path('arrange/', include('arrangeTheCats.urls')),
     path('', include('CetApp.urls')),
