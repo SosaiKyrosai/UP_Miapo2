218d3a0adb3b7d963ea08698a2825ad66705df7b
SosaiKyrosai <119391495+SosaiKyrosai@users.noreply.github.com>
2023-04-20 18:16:29 +0700

    Update urls.py
    Пути пофикшены

--- Cet/Cet/urls.py
+++ Cet/Cet/urls.py
@@ -3,23 +3,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('generate/', include('generateName.urls')),
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+    path('generate/', include('generateName.urls')),
     path('estimate/', include('estimateName.urls')),
-<<<<<<< HEAD
-    # path('pat/', include('patCat.urls')),
-    # path('arrange/', include('arrangeTheCats.urls')),
-=======
-    # path('estimate/', include('estimateName.urls')),
     path('pat/', include('patCat.urls')),
     path('arrange/', include('arrangeTheCats.urls')),
->>>>>>> pat
-=======
-    path('pat/', include('patCat.urls')),
-    path('arrange/', include('arrangeTheCats.urls')),
->>>>>>> 4d7cadb08f4e015747dc5c20f5ed0c0434491b54
->>>>>>> estimate
     path('', include('CetApp.urls')),
 ]
